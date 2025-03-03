adbb4548f60cc4e6b634b20ce38d719022394968
Sandunethmini <167853535+Sandunethmini@users.noreply.github.com>
2025-03-03 10:13:54 +0530

    Add files via upload

---
+++ 22IT0548_Library Management System/FictionBook.java
@@ -0,0 +1,5 @@
+public class FictionBook extends Book {
+    public FictionBook(String title, String author, String isbn, boolean availability) {
+        super(title, author, isbn, availability);
+    }
+}
---
+++ 22IT0548_Library Management System/NonFictionBook.java
@@ -0,0 +1,5 @@
+public class NonFictionBook extends Book {
+    public NonFictionBook(String title, String author, String isbn, boolean availability) {
+        super(title, author, isbn, availability);
+    }
+}
